ffe9c2c37eed42a4fcd82cbec3fada951b44ef44
MinavKaria <minavpkaria@gmail.com>
2023-09-19 01:10:12 +0530

    Initialize Hero.jsx

--- Datazen-Website/src/components/Hero.jsx
+++ Datazen-Website/src/components/Hero.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import './Hero.css'; 
 
 function Hero() 
 {
   return (
     <>
-    <h1>Hello</h1>
+    <div className='layer-1'>
+    <svg xmlns="http://www.w3.org/2000/svg" width="784" height="468" viewBox="0 0 715 468" fill="none">
+      <path d="M-69 134.764V0L715 468H-69V134.764Z" fill="url(#paint0_linear_177_639)"/>
+      <defs>
+            <linearGradient id="paint0_linear_177_639" x1="323" y1="0" x2="323" y2="468" gradientUnits="userSpaceOnUse">
+                  <stop stop-color="#511616" stop-opacity="0.9"/>
+                  <stop offset="1" stop-color="#AB011A"/>
+            </linearGradient>
+      </defs>
+    </svg>
+    </div>
+
+
+    {/* width: 816px;height: 602px; */}
     </>
   )
   
 }
 
 export default Hero
+
+

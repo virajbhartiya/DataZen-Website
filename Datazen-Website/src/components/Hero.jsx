942db37b560a53824203e37a46555a1d88dbc784
MinavKaria <minavpkaria@gmail.com>
2023-09-19 14:29:09 +0530

    Changes in Navbar and Hero

--- Datazen-Website/src/components/Hero.jsx
+++ Datazen-Website/src/components/Hero.jsx
@@ -1,24 +1,14 @@
 import React from 'react'
 import './Hero.css'; 
+import DataZen_Logo from '../imgs/DataZen_Logo.png';
 
 function Hero() 
 {
   return (
     <>
-    <div className='layer-1'>
-    <svg xmlns="http://www.w3.org/2000/svg" width="784" height="468" viewBox="0 0 715 468" fill="none">
-      <path d="M-69 134.764V0L715 468H-69V134.764Z" fill="url(#paint0_linear_177_639)"/>
-      <defs>
-            <linearGradient id="paint0_linear_177_639" x1="323" y1="0" x2="323" y2="468" gradientUnits="userSpaceOnUse">
-                  <stop stop-color="#511616" stop-opacity="0.9"/>
-                  <stop offset="1" stop-color="#AB011A"/>
-            </linearGradient>
-      </defs>
-    </svg>
-    </div>
-
-
-    {/* width: 816px;height: 602px; */}
+      <div className='container'>
+        <img src={DataZen_Logo} alt="" srcset="" className='Datazen_logo'/>
+      </div>
     </>
   )
   
--- Datazen-Website/src/components/Navbar.jsx
+++ Datazen-Website/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Navbar.css'; 
 import SomaiyaLogo from '../imgs/Somaiya_logo.png'; 
+import menu_icon from '../imgs/menu-icon.svg';
 
 function Navbar() 
 {
@@ -8,13 +9,19 @@ function Navbar()
     <>
     <div className='container'>
     <nav className="navbar">
+      <div className='menu'>
+      <button>
+            <img src={menu_icon} alt="" srcset="" className='menu-icon'/>
+      </button>
+        
+      </div>
       <div className="navbar-center">
         <a className='nav_link'>Home</a>
         <a>About</a>
         <a >Events</a>
         <a >Team</a>
       </div> 
-      <div>
+      <div className='logo'>
 
       <img src={SomaiyaLogo} alt="" srcset=""  className='Somaiya_logo'/>
       

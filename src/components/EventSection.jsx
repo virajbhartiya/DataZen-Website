09410f0c2eb3fa2f8ac94ed27cbf8dcd040acb07
virajbhartiya <vlbhartiya@gmail.com>
2024-01-03 00:38:12 +0530

    Added Datathon event

--- src/components/EventSection.jsx
+++ src/components/EventSection.jsx
@@ -1,34 +1,49 @@
 import React from 'react'
-import {Link} from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import '../styles/EventSection.css'
 
 
-function EventSection({isLeft,title,content,linker,imageLink}) 
-{
-    
+function EventSection({ isLeft, title, content, linker, imageLink }) {
+  if (linker) {
+
+    React.useEffect(() => {
+      const script = document.createElement('script');
+      script.src = 'https://apply.devfolio.co/v2/sdk.js';
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      return () => {
+        document.body.removeChild(script);
+      }
+    }, []);
+  }
+
   return (
     <div className='event-section-container'>
-       <b><h3 className='event-title'>{title}</h3></b>
-         <div className={`${!isLeft? 'right' : 'left'}`}  >
-            <div className='event-image'>
-                <img src={imageLink} className='event-img'/>
+      <b><h3 className='event-title'>{title}</h3></b>
+      <div className={`${!isLeft ? 'right' : 'left'}`}  >
+        <div className='event-image'>
+          <img src={imageLink} className='event-img' />
 
 
-            </div>
-            
-            <div className="event-content-box">
-             
-               
-                <div className='event-content'>
-                <p>{content}</p>
-                </div>
+        </div>
 
-                
-                
-                {linker && <Link to={linker} className='register'>Register</Link>}
-            </div>
-            </div>
-        
+        <div className="event-content-box">
+
+          <div className='event-content'>
+            <p>{content}</p>
+          </div>
+          {linker &&
+            <div
+              className="apply-button"
+              data-hackathon-slug="datathon-3"
+              data-button-theme="dark"
+            ></div>
+          }
+          {/* {linker && <Link to={linker} className='register'>Register</Link>} */}
+        </div>
+      </div>
+
     </div>
   )
 }
